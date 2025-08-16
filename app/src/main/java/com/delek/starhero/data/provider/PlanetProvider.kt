b55116c16c2be50494b3e1139f71feedc7fcdf92
manolek1975 <manolek1975@gmail.com>
2025-08-16 12:43:08 +0200

    Planet images

--- app/src/main/java/com/delek/starhero/data/provider/PlanetProvider.kt
+++ app/src/main/java/com/delek/starhero/data/provider/PlanetProvider.kt
@@ -9,30 +9,11 @@ class PlanetProvider {
     companion object{
         fun loadPlanets(context: Context): List<PlanetEntity> {
             val planets = mutableListOf<PlanetEntity>()
-            val names = context.resources.getStringArray(R.array.name_stars)
-            val types = context.resources.getStringArray(R.array.type_stars)
+            val names = context.resources.getStringArray(R.array.name_planets)
+            val images = context.resources.getStringArray(R.array.image_planets)
 
             for (i in names.indices) {
-                if(types[i] == "S"){
-                    for (s in 1..4) {
-                        planets.add(PlanetEntity(i+s, names[i], "", types[i], i, i, 0, 0))
-                    }
-                }
-                if(types[i] == "A"){
-                    for (a in 5..7) {
-                        planets.add(PlanetEntity(i+a, names[i], "", types[i], i, i, 0, 0))
-                    }
-                }
-                if(types[i] == "R"){
-                    for (r in 8..13) {
-                        planets.add(PlanetEntity(i+r, names[i], "", types[i], i, i, 0, 0))
-                    }
-                }
-                if(types[i] == "B"){
-                    for (b in 14..19) {
-                        planets.add(PlanetEntity(i+b, names[i], "", types[i], i, i, 0, 0))
-                    }
-                }
+                planets.add(PlanetEntity(i+1, names[i], images[i], "", 0, 0, 0, 0))
             }
             return planets
         }
